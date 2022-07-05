55e6a2d2318cf009f3f09a79b28e09966ffb0e9a
buseyolsal <buseyolsal@gmail.com>
2022-07-05 22:57:00 +0300

    Öğrenci Classı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba dünya");
+
+        System.out.println("MErhaba dünya 2");
+        System.out.println("Merhaba dünya 3");
     }
 }
